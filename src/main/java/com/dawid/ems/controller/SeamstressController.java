799272a85cb103ba77ad7dc1c12b2b436644ba24
infarot <infarot@gmail.com>
2019-02-15 14:43:36 +0100

    code refactor

--- src/main/java/com/dawid/ems/controller/SeamstressController.java
+++ src/main/java/com/dawid/ems/controller/SeamstressController.java
@@ -53,16 +53,15 @@ public class SeamstressController {
     public Double getAverageResult(@PathVariable int seamstressId) {
         List<Result> results = seamstressService.getAllResults(seamstressId);
         //get all results from one day
-        HashMap<LocalDate, List<Result>> map = results.stream().collect(
+        Map<LocalDate, List<Result>> map = results.stream().collect(
                 Collectors.groupingBy(Result::getDate, HashMap::new, Collectors.toList()));
         //sum all results from one day
-        HashMap<LocalDate, Double> percentageResultMap = new HashMap<>();
+        Map<LocalDate, Double> percentageResultMap = new HashMap<>();
         for (LocalDate d : map.keySet()) {
             percentageResultMap.put(d, map.get(d).stream().mapToDouble(Result::getPercentageResult).sum());
         }
         //calculate average and ignore results which are less then 50 %
         OptionalDouble average = percentageResultMap.values().stream().filter(a -> a > 50).mapToDouble(a -> a).average();
-
         if (average.isPresent()) {
             return average.getAsDouble();
         } else {
@@ -74,5 +73,4 @@ public class SeamstressController {
     public Double getScore(@PathVariable int seamstressId){
         return seamstressService.getAllResults(seamstressId).stream().mapToDouble(Result::getPercentageResult).sum();
     }
-
 }
