71db3c420f502fb5cab2f0a31e3bde386df42e06
infarot <infarot@gmail.com>
2019-02-15 08:35:26 +0100

    added json ignore for results in seamstress

--- src/main/java/com/dawid/ems/entity/Seamstress.java
+++ src/main/java/com/dawid/ems/entity/Seamstress.java
@@ -1,5 +1,7 @@
 package com.dawid.ems.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -23,6 +25,7 @@ public class Seamstress {
         this.lastName = lastName;
     }
 
+    @JsonIgnore
     public List<Result> getResults() {
         return results;
     }
