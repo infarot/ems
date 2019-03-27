d0060b6369483c9ac7bce075d86d86eab20278f2
infarot <infarot@gmail.com>
2019-03-27 09:03:14 +0100

    fixed quilting controller test

--- src/test/java/com/dawid/ems/controllerTest/QuiltingControllerSpringBootTest.java
+++ src/test/java/com/dawid/ems/controllerTest/QuiltingControllerSpringBootTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,7 +26,9 @@ import org.springframework.web.context.WebApplicationContext;
 import java.time.LocalDate;
 import java.util.Collections;
 
+import static org.mockito.Mockito.spy;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,21 +43,13 @@ public class QuiltingControllerSpringBootTest {
     @MockBean
     private QuiltingService quiltingService;
 
-    private ProductionWorker productionWorker;
-
     private MockMvc mvc;
 
     @Before
     public void setup() {
-        productionWorker = new ProductionWorker("test", "test");
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
+        ProductionWorker productionWorker = new ProductionWorker("test", "test");
+        productionWorker.setId(53);
 
-    @Test
-    public void canGetAllQuiltingData() throws Exception {
         QuiltingData quiltingData = new QuiltingData();
         quiltingData.setOperator(productionWorker);
         quiltingData.setDate(LocalDate.of(2019, 3, 15));
@@ -63,9 +58,22 @@ public class QuiltingControllerSpringBootTest {
         quiltingData.setQuiltedIndices(Collections.singletonList(
                 new QuiltedIndex("MALFORS_80", 200, 2, productionWorker, quiltingData, 1)));
 
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
         Mockito.when(quiltingService.getAll()).thenReturn(Collections.singletonList(quiltingData));
 
+        Mockito.when(quiltingService.getAllByDateBetweenAndOperator(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 5, 22), productionWorker)).thenReturn(Collections.singletonList(quiltingData));
+
+    }
+
+    @Test
+    public void canGetAllQuiltingData() throws Exception {
+
         mvc.perform(MockMvcRequestBuilders.get("/api/quilting").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[0].id", Matchers.is(1)))
                 .andExpect(jsonPath("$.[0].operator.name", Matchers.is("test")))
@@ -74,17 +82,9 @@ public class QuiltingControllerSpringBootTest {
 
     @Test
     public void canGetQuiltingDataByDateIntervalAndOperator() throws Exception {
-        QuiltingData quiltingData = new QuiltingData();
-        quiltingData.setOperator(productionWorker);
-        quiltingData.setDate(LocalDate.of(2019, 3, 15));
-        quiltingData.setId(1);
-        quiltingData.setQuilterStatistics(new QuilterStatistics(202.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0));
-        quiltingData.setQuiltedIndices(Collections.singletonList(
-                new QuiltedIndex("MALFORS_80", 200, 2, productionWorker, quiltingData, 1)));
 
-        Mockito.when(quiltingService.getAllByDateBetweenAndOperator(LocalDate.of(2019,3,22),LocalDate.of(2019,3,22), productionWorker)).thenReturn(Collections.singletonList(quiltingData));
-
-        mvc.perform(MockMvcRequestBuilders.get("/api/quilting/2019-03-22/2019-03-22/53").contentType(MediaType.APPLICATION_JSON))
+        mvc.perform(MockMvcRequestBuilders.get("/api/quilting/2019-03-01/2019-03-22/53").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[0].id", Matchers.is(1)))
                 .andExpect(jsonPath("$.[0].operator.name", Matchers.is("test")))
